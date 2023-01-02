c5171720f9f3857e03ed522e2438b08f0c9c2761
Robert Hagen <hagen.robert@protonmail.com>
2023-01-02 22:17:45 +0100

    day 04: high-entropy passphrases

---
+++ Day04/Program.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let duplicates (p: string) =
+    p.Split " " |> Array.countBy id |> Array.forall (fun (_, v) -> v <= 1)
+
+let anagrams (p: string) =
+    let sortString (s: string) =
+        s.ToCharArray() |> Array.sort |> Array.map string |> String.concat ""
+
+    p.Split " "
+    |> Array.map sortString
+    |> Array.countBy id
+    |> Array.forall (fun (_, v) -> v <= 1)
+
+let passphrases = File.ReadAllLines "d04.in"
+
+let p1 = passphrases |> Array.filter duplicates |> Array.length
+printfn $"Part 1: {p1}"
+
+let p2 = passphrases |> Array.filter duplicates |> Array.filter anagrams |> Array.length
+printfn $"Part 2: {p2}"
